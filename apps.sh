69eac176c32a488e1940b111b83f40cd7b995481
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-01 20:35:48 +0300

    adds scripts in module-2

--- apps.sh
+++ apps.sh
@@ -3,6 +3,6 @@ echo 'Running App-1'
 echo 'Module-1'
 python src/app1/module1/script1.py
 python src/app1/module1/script2.py
-# echo 'Module-2'
-# python src/app1/module1/script1.py
-# python src/app1/module1/script2.py
+echo 'Module-2'
+python src/app1/module2/script1.py
+python src/app1/module2/script2.py
---
+++ src/app1/module2/script2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+class Class_Script:
+    @staticmethod
+    def Say_Hi():
+        try:
+            message = f'Hi from {Class_Script.Say_Hi.__name__} from {Class_Script.__name__}'
+        except Exception:
+            return None
+        else:
+            return message
+
+
+def Main():
+    print(f'This is the {Main.__name__}() function from Script-2')
