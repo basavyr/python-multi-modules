0f34390f118e2afe1ea09a06629cd5253e7648c6
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-01 19:40:37 +0300

    add function that uses numpy within script 2

--- src/app1/module1/script2.py
+++ src/app1/module1/script2.py
@@ -2,6 +2,8 @@
 
 import os
 import platform
+import numpy as np
+from numpy import random as rd
 
 
 class Class_Script:
@@ -34,10 +36,21 @@ class Class_Script:
         else:
             return full_pc_name
 
+    @staticmethod
+    def Give_Random_Array(size):
+        get_random = lambda: rd.randint(0, 10)
+        try:
+            classic_array = [get_random() for _ in range(size)]
+            np_array = np.array(classic_array)
+        except Exception:
+            return 0
+        else:
+            return np_array
+
 
 def Main():
     print(f'This is the {Main.__name__}() function from Script-2')
-
+    print(Class_Script.Give_Random_Array(10))
 
 if __name__ == '__main__':
     Class_Script.Say_Hi()
