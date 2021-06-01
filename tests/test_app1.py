5bbf4186d01c2832bef6f967c48be1450e6f4c2b
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-01 16:50:49 +0300

    add proper test for script 2

--- tests/test_app1.py
+++ tests/test_app1.py
@@ -7,9 +7,11 @@ app = 'App-1'
 try:
     sys.path.append('../')
     import src.app1.module1.script1 as script1
+    import src.app1.module1.script2 as script2
 except ImportError:
     sys.path.append('src/')
     import app1.module1.script1 as script1
+    import app1.module1.script2 as script2
 finally:
     print(f'\nImporting modules from {app} 📚\n')
 
@@ -20,7 +22,6 @@ class Test_Script1:
     def test_sayhi():
         x = script1.Class_Script.Say_Hi()
         try:
-            print(f'⚙️ Testing Script-1...')
             assert x != 0
         except AssertionError:
             print('Script-1: Failed ❌')
@@ -42,7 +43,7 @@ class Test_Script1:
         test_good_list = [x for x in range(1, 10)]
         test_bad_list = [1, 2, 3, 'a']
         try:
-            x=script1.Class_Script.Compute_Term(test_good_list)
+            x = script1.Class_Script.Compute_Term(test_good_list)
             assert x != 0
         except AssertionError:
             print('Script-1: Failed ❌')
@@ -56,8 +57,29 @@ class Test_Script1:
         Test_Script1.test_computeterms()
 
 
+class Test_Script2:
+
+    @staticmethod
+    def test_sayhi():
+        x = script2.Class_Script.Say_Hi()
+        try:
+            assert x != 0
+        except AssertionError:
+            print('Script-2: Failed ❌')
+        else:
+            print('Script-2: Success ✅')
+
+    @staticmethod
+    def Start_Test():
+        Test_Script2.test_sayhi()
+
+
 def Main():
+    print(f'\n⚙️ Testing Script-1...\n')
     Test_Script1.Start_Test()
+    
+    print(f'\n⚙️ Testing Script-2...\n')
+    Test_Script2.Start_Test()
 
 
 if __name__ == '__main__':
