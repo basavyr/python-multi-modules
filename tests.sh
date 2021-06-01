562165e9c2c3adc881d5fd64bf48cde48b8e461f
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-01 19:28:55 +0300

    update the pipeline for app-1 testing

--- tests.sh
+++ tests.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+system=$(uname -a)
+
+echo 'Running the test script on:'
+echo $system
+echo 
 python tests/test_app1.py
--- tests/test_app1.py
+++ tests/test_app1.py
@@ -8,12 +8,12 @@ try:
     sys.path.append('../')
     import src.app1.module1.script1 as script1
     import src.app1.module1.script2 as script2
-    print('Direct execution of the test')
+    #print('Direct execution of the test')
 except ImportError:
     sys.path.append('src/')
     import app1.module1.script1 as script1
     import app1.module1.script2 as script2
-    print('Execution of the script from tests/ dir')
+    #print('Execution of the script from tests/ dir')
 finally:
     print(f'\nImporting modules from {app} -> OK ✅\n')
 
