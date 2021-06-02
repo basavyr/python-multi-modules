dfb7b4670f2d46c0322dff945d4334e1257d693d
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-02 20:38:42 +0300

    add main test for app2 module 2

--- tests/test_app2.py
+++ tests/test_app2.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import sys
 
 app = 'App-2'
@@ -11,23 +12,30 @@ except ModuleNotFoundError:
     sys.path.insert(1, '../')
     from src.app2.module2 import script1
     from src.app2.module2 import script2
+finally:
+    print(f'\nImporting modules from {app} -> OK ✅\n')
 
 
 class Test_Module2_Script1:
 
     @staticmethod
     def test_main():
+        ok_msg = f'Test @ {datetime.utcnow()}: ✅'
+        fail_msg = f'Test @ {datetime.utcnow()}: ❌'
         try:
             x = script1.Main()
-            print(x)
         except Exception:
-            print('Test failed ❌')
+            print(fail_msg)
         else:
-            print('Test passed ✅')
+            print(ok_msg)
+
+    @staticmethod
+    def Start_Test():
+        Test_Module2_Script1.test_main()
 
 
 def Main():
-    print('Main works')
+    Test_Module2_Script1.Start_Test()
 
 
 if __name__ == '__main__':
