83434e425b4bd85afa5d6348884a7651b7468a4b
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-02 16:12:17 +0300

    add test class for app 2

--- tests/test_app2.py
+++ tests/test_app2.py
@@ -9,4 +9,22 @@ except ModuleNotFoundError:
     from src.app2.module2 import script1
     from src.app2.module2 import script2
 
-script1.Main()
+
+class Test_Module2_Script1:
+
+    @staticmethod
+    def test_main():
+        try:
+            x = script1.Main()
+        except Exception:
+            print('Test failed ❌')
+        else:
+            print('Test passed ✅')
+
+
+def Main():
+    print('Main works')
+
+
+if __name__ == '__main__':
+    Main()
