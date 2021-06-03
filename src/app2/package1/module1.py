5116794963c4b51062a191b1ee59db7c5914dc7c
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-03 08:42:58 +0300

    adds exra function to module1 package 2

--- src/app2/package1/module1.py
+++ src/app2/package1/module1.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
 
 import os
+import numpy as np
 
-def Test():
-    return f'function {Test.__name__}() from  {os.path.basename(__file__)} inside Module-1'
+
+class Class_Script:
+    @staticmethod
+    def Test():
+        mseg = f'function {Class_Script.Test.__name__}() from  {os.path.basename(__file__)} inside Module-1'
+        # print(mseg)
+        return mseg
+
+    @staticmethod
+    def Extra_Text():
+        x = np.arange(0, 11, 1)
+        y = list(map(lambda x: x * 2, x))
+        assert len(x) == len(y), 'Arrays do not have the same length'
+        return x, y
+
+
+def Main():
+    Class_Script.Test()
+
+
+if __name__ == '__main__':
+    Main()
