505cedc4bd84a658a331d54ca8d4353ec5a3ca02
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-03 10:22:19 +0300

    imports the app-3 into a test module

--- tests/test_app3.py
+++ tests/test_app3.py
@@ -1,1 +1,14 @@
-print('will test the app-3')
+import sys
+
+app = 'App=3'
+
+try:
+    sys.path.insert(1, '../')
+    import src.app3.package1 as pckg_1
+except ModuleNotFoundError:
+    sys.path.insert(1, '')
+    import src.app3.package1 as pckg_1
+finally:
+    print(f'\nImporting modules from {app} -> OK ✅\n')
+
+print(pckg_1)
