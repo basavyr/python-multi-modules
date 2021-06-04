62c9299bad62817167003664fca5f1e2640fc44a
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-04 20:46:13 +0300

    add test for module-1 package-2

--- tests/test_app2.py
+++ tests/test_app2.py
@@ -88,23 +88,36 @@ class Test_Package2_Module1:
 
     @staticmethod
     def test_main():
-        ok_msg = f'Test @ {datetime.utcnow()}: ✅'
-        fail_msg = f'Test @ {datetime.utcnow()}: ❌'
+        fail_msg = f'Test: {Test_Package2_Module1.test_main.__name__} @ {datetime.utcnow()}: ❌'
+        ok_msg = f'Test: {Test_Package2_Module1.test_main.__name__} @ {datetime.utcnow()}: ✅'
         try:
-            x = module1_p2.Main()
+            x = module1_p2.Class_Script.Test()
         except Exception:
             print(fail_msg)
         else:
             print(ok_msg)
 
-    @staticmethod
+    @ staticmethod
+    def test_givearray():
+        try:
+            x = module1_p2.Class_Script.Give_Array()
+            assert type(x) != None
+        except AssertionError:
+            print(
+                f'Test: {Test_Package2_Module1.test_givearray.__name__} @ {datetime.utcnow()}: ❌')
+        else:
+            print(
+                f'Test: {Test_Package2_Module1.test_givearray.__name__} @ {datetime.utcnow()}: ✅')
+
+    @ staticmethod
     def Start_Test():
         Test_Package2_Module1.test_main()
+        Test_Package2_Module1.test_givearray()
 
 
 class Test_Package2_Module2:
 
-    @staticmethod
+    @ staticmethod
     def test_givearray():
         try:
             x = module2_p2.Class_Script.Give_Array()
@@ -116,7 +129,7 @@ class Test_Package2_Module2:
             print(
                 f'Test: {Test_Package2_Module2.test_givearray.__name__} @ {datetime.utcnow()}: ✅')
 
-    @staticmethod
+    @ staticmethod
     def test_gettime():
         try:
             x = module2_p2.Class_Script.Get_Time()
@@ -128,16 +141,24 @@ class Test_Package2_Module2:
             print(
                 f'Test: {Test_Package2_Module2.test_gettime.__name__} @ {datetime.utcnow()}: ✅')
 
-    @staticmethod
+    @ staticmethod
     def Start_Test():
         Test_Package2_Module2.test_givearray()
         Test_Package2_Module2.test_gettime()
 
 
 def Main():
+
+    print(f'********** Package-1 **********')
+    print(f'\n⚙️ Testing Module-1...\n')
     Test_Package1_Module1.Start_Test()
+    print(f'\n⚙️ Testing Module-2...\n')
     Test_Package1_Module2.Start_Test()
-    # Test_Package2_Module1.Start_Test()
+
+    print(f'\n********** Package-2 **********')
+    print(f'\n⚙️ Testing Module-1...\n')
+    Test_Package2_Module1.Start_Test()
+    print(f'\n⚙️ Testing Module-2...\n')
     # Test_Package2_Module2.Start_Test()
 
 
