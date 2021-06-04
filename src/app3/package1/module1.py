ec6ff6eabb4e8b2910e9a868cb4dc0dfd40005d4
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-04 21:23:54 +0300

    app3 has robust testing module for all the methods

--- src/app3/package1/module1.py
+++ src/app3/package1/module1.py
@@ -11,34 +11,46 @@ class Export_Object:
     object_info_file = 'object_info.md'
 
     @staticmethod
-    def Import_Object(x_obj):
+    def Import_Object(obj):
         try:
-            assert x_obj != None
+            assert obj != None
         except AssertionError:
             return None
         else:
-            return x_obj
-        # print(f'Object has been imported -> {x_obj}')
+            return obj
 
     @staticmethod
     def Get_Classes(obj):
+        try:
+            assert obj != None
+        except AssertionError:
+            return None
         class_members = inspect.getmembers(obj, inspect.isclass)
         return class_members
 
     @staticmethod
     def Get_Functions(obj):
+        try:
+            assert obj != None
+        except AssertionError:
+            return None
         function_members = inspect.getmembers(obj, inspect.isfunction)
         return function_members
 
 
-if __name__ == '__main__':
+def Main():
     test_object = ['a string', f'a number -> {4}']
+    fake_test = None
     try:
         x = Export_Object.Import_Object(test_object)
         y = Export_Object.Get_Classes(test_object)
         z = Export_Object.Get_Functions(test_object)
-        assert type(x) != None and type(y) != None and type(z) != None
+        assert x != None and y != None and z != None, "Invalid results for x,y,z"
     except AssertionError:
         print(f'Failed running {os.path.basename(__file__)} ❌')
     else:
         print(f'Running {os.path.basename(__file__)} successfully ✅')
+
+
+if __name__ == '__main__':
+    Main()
