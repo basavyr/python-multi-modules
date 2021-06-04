86ca546e35d935aee1d21d7d6cb6499f08654471
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-06-04 11:55:10 +0300

    Revert "removed the import of module1 from package 1 in app2"
    This reverts commit c433368b2ce668e2bf5b56e80b1408894f848447.

--- apps.sh
+++ apps.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-# echo 'Running App-1 🚀'
-
-# echo 'Package-1 📦'
-# python src/app1/package1/module1.py
-# python src/app1/package1/module2.py
-
-# echo 'Package-2 📦'
-# python src/app1/package2/module1.py
-# python src/app1/package2/module2.py
-
-echo 'Running App-2 🚀'
+echo 'Running App-1 🚀'
 
 echo 'Package-1 📦'
-python src/app2/package2/module1.py
-python src/app2/package2/module2.py
+python src/app1/package1/module1.py
+python src/app1/package1/module2.py
+
+echo 'Package-2 📦'
+python src/app1/package2/module1.py
+python src/app1/package2/module2.py
--- src/app2/package2/module1.py
+++ src/app2/package2/module1.py
@@ -8,10 +8,10 @@ import sys
 
 # uses an external module, from package 1
 try:
-    from src.app2.package1 import module2 as m2
+    from src.app2.package1 import module1 as m1
 except ModuleNotFoundError:
-    # sys.path.insert(1, '../package1')
-    import module2 as m2
+    sys.path.insert(1, '../')
+    from package1 import module1 as m1
 
 
 class Class_Script:
@@ -27,8 +27,7 @@ class Class_Script:
 
 def Main():
     message = f'This is {os.path.basename(__file__)} script from Module-2'
-    print(f'Successfully imported module -> {dir(m2)}')
-    print(message)
+    print(f'Successfully imported module -> {dir(m1)}')
 
 
 if __name__ == '__main__':
